0e31e81ea641a97120307e2c9db4905c622b9fe9
Angel Morales Karam <a.jesse3001@gmail.com>
2017-08-12 22:32:49 -0500

    create a custom collection view  layout

---
+++ AJMListApp/AJMLayout.swift
@@ -0,0 +1,13 @@
+//
+//  AJMLayout.swift
+//  AJMListApp
+//
+//  Created by Angel Jesse Morales Karam Kairuz on 12/08/17.
+//  Copyright © 2017 TheKairuzBlog. All rights reserved.
+//
+
+import UIKit
+
+class AJMLayout: UICollectionViewLayout {
+
+}
