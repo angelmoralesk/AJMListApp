abafa2daf902f2312de0bd5f61cb0221da4594e7
Angel Morales Karam <a.jesse3001@gmail.com>
2017-09-05 15:48:18 -0500

    create a new layout that simplifies the 3D layout stack cells

---
+++ AJMFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  AJMFlowLayout.swift
+//  AJMListApp
+//
+//  Created by Angel Jesse Morales Karam Kairuz on 05/09/17.
+//  Copyright © 2017 TheKairuzBlog. All rights reserved.
+//
+
+import UIKit
+
+class AJMFlowLayout: UICollectionViewFlowLayout {
+
+}
