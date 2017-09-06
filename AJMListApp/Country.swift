5fe29d4153014527985999510531e570def85cfb
Angel Morales Karam <a.jesse3001@gmail.com>
2017-09-06 13:18:44 -0500

    creating a new class that will hold our model

---
+++ AJMListApp/Country.swift
@@ -0,0 +1,9 @@
+//
+//  Country.swift
+//  AJMListApp
+//
+//  Created by Angel Jesse Morales Karam Kairuz on 06/09/17.
+//  Copyright © 2017 TheKairuzBlog. All rights reserved.
+//
+
+import Foundation
