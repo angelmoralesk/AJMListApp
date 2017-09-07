60f3556ff1d4b9b9880644423a1734ddfc11b8d2
Angel Morales Karam <a.jesse3001@gmail.com>
2017-09-07 16:12:06 -0500

    implementing collectionviewattributes

--- AJMListApp/CountryCell.swift
+++ AJMListApp/CountryCell.swift
@@ -12,4 +12,11 @@ class CountryCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    
+    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+        super.apply(layoutAttributes)
+        let circularlayoutAttributes = layoutAttributes as! CircularCollectionViewLayoutAttributes
+        self.layer.anchorPoint = circularlayoutAttributes.anchorPoint
+        self.center.y += (circularlayoutAttributes.anchorPoint.y - 0.5) * self.bounds.height
+    }
 }
