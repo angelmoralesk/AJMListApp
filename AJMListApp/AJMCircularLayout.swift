b79285c3458d0f4d5a4c0c9c5193c88c76f3a146
Angel Morales Karam <a.jesse3001@gmail.com>
2017-09-07 16:10:04 -0500

    implementing a custom attributes class

--- AJMListApp/AJMCircularLayout.swift
+++ AJMListApp/AJMCircularLayout.swift
@@ -8,6 +8,29 @@
 
 import UIKit
 
+class CircularCollectionViewLayoutAttributes : UICollectionViewLayoutAttributes {
+    
+    var anchorPoint = CGPoint(x: 0.5, y: 0.5)
+    var angle : CGFloat = 0 {
+        didSet {
+            zIndex = Int(angle * 1000000)
+            transform = CGAffineTransform(rotationAngle: angle)
+
+        }
+    }
+    
+    override func copy(with zone: NSZone? = nil) -> Any {
+        let copiedAttributes: CircularCollectionViewLayoutAttributes = super.copy(with: zone) as! CircularCollectionViewLayoutAttributes
+        copiedAttributes.anchorPoint = self.anchorPoint
+        copiedAttributes.angle = self.angle
+        return copiedAttributes
+    }
+    
+    
+    
+}
+
+
 class AJMCircularLayout: UICollectionViewLayout {
 
 }
