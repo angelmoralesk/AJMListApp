5db6d4080c63fc24349af39e4f8c71cd55d78440
Angel Morales Karam <a.jesse3001@gmail.com>
2017-09-07 16:11:11 -0500

    implementing CircularEuropeVisualizerVC

--- AJMListApp/CircularEuropeVisualizerViewController.swift
+++ AJMListApp/CircularEuropeVisualizerViewController.swift
@@ -10,26 +10,38 @@ import UIKit
 
 class CircularEuropeVisualizerViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var names = ["berlin-cathedral","brandenburg-gate","colosseum", "eiffel", "notre-dame", "venice"]
+    var places : [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        names = names.map { $0.appending(".jpg") }
+        collectionView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI_2))
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension CircularEuropeVisualizerViewController : UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CountryCell
+        let name = "\(names[indexPath.row])"
+        cell.imageView.image = UIImage(named: name)
+        cell.imageView.contentMode = .scaleAspectFit
+        cell.imageView.transform =  CGAffineTransform(rotationAngle: CGFloat(-M_PI/2))
+        return cell
+    }
+    
 }
+
